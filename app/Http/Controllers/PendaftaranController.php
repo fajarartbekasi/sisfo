8c0f44cf86ace7a1b9ebff3b34fcb01b062bb50e
fajarartbekasi <fajarbekasieditting@gmail.com>
2020-11-24 09:37:53 +0700

    install nexmo and nexmo client and change code to send sms gateway

--- app/Http/Controllers/PendaftaranController.php
+++ app/Http/Controllers/PendaftaranController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Pendaftaran;
 use Illuminate\Http\Request;
-
+use Nexmo\Laravel\Facade\Nexmo;
 class PendaftaranController extends Controller
 {
     public function index()
@@ -18,9 +18,46 @@ class PendaftaranController extends Controller
         return view('pendaftaran.create');
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        $pendaftaran = Pendaftaran::create($this->validateRequest());
+        $this->validate($request,[
+            'pilihan_masuk'  => 'required',
+            'nama'           => 'required',
+            'jenis_kelamin'  => 'required',
+            'kelas'          => 'required',
+            'jumlah_sodara'  => 'required',
+            'alamat'         => 'required',
+            'ayah'           => 'required',
+            'ibu'            => 'required',
+            'pekerjaan_ibu'  => 'required',
+            'pekerjaan_ayah' => 'required',
+            'alamat_orangtua'=> 'required',
+            'no_telp'        => 'required',
+            'status'         => 'required',
+        ]);
+        Nexmo::message()->send([
+            'to'   => $request->get('no_telp'),
+            'from' => '16105552344',
+            'text' => 'Assallamuallaikum.wr.wb'.$request->get('nama').
+                      'kami dari TPQ Masjid Ziaadaturahman ingin memberitahukan bahwa pendaftaran kamu telah diterima
+                      harap melakukan test seleksi untuk tahap berikut nya untuk informasi test
+                      akan kami beritahukan di situs web kami dengan cara cek kegiatan terimakasih.'
+        ]);
+        $pendaftaran = Pendaftaran::create([
+            'pilihan_masuk'  => $request->input('pilihan_masuk'),
+            'nama'           => $request->input('nama'),
+            'jenis_kelamin'  => $request->input('jenis_kelamin'),
+            'kelas'          => $request->input('kelas'),
+            'jumlah_sodara'  => $request->input('jumlah_sodara'),
+            'alamat'         => $request->input('alamat'),
+            'ayah'           => $request->input('ayah'),
+            'ibu'            => $request->input('ibu'),
+            'pekerjaan_ibu'  => $request->input('pekerjaan_ibu'),
+            'pekerjaan_ayah' => $request->input('pekerjaan_ayah'),
+            'alamat_orangtua'=> $request->input('alamat_orangtua'),
+            'no_telp'        => $request->input('no_telp'),
+            'status'         => $request->input('status'),
+        ]);
 
         return redirect()->back()->with(['success' => 'Pendaftaran kamu telah dikirim terimakasih']);
     }
@@ -31,27 +68,22 @@ class PendaftaranController extends Controller
 
         return view('pendaftaran.show', compact('pendaftaran'));
     }
-    public function update(Pendaftaran $pendaftaran)
+    public function update(Request $request, $id)
     {
-        $pendaftaran->update($this->validateRequest());
+        $pendaftaran = Pendaftaran::findOrFail($id);
+
+        Nexmo::message()->send([
+            'to'   => $pendaftaran->no_telp,
+            'from' => '16105552344',
+            'text' => 'Assallamuallaikum.wr.wb'
+                      .$pendaftaran->nama.
+                      'kami dari TPQ Masjid Ziaadaturahman ingin memberitahukan bahwa hasil seleksi kamu dinyatakan'
+                      .$pendaftaran->status.
+                      'terimakasih telah melakukan pendaftaran dan mengikuti test di TPQ Masjid Ziaadaturahman semoga kamu terus semangat terimakasih'
+        ]);
+
+        $pendaftaran->update($request->all());
 
         return redirect()->back()->with(['success' => 'Pendaftaran berhasil di perbarui']);
-    }
-    private function validateRequest(){
-        return request()->validate([
-           'pilihan_masuk'  => 'required',
-           'nama'           => 'required',
-           'jenis_kelamin'  => 'required',
-           'kelas'          => 'required',
-           'jumlah_sodara'  => 'required',
-           'alamat'         => 'required',
-           'ayah'           => 'required',
-           'ibu'            => 'required',
-           'pekerjaan_ibu'  => 'required',
-           'pekerjaan_ayah' => 'required',
-           'alamat_orangtua'=> 'required',
-           'no_telp'        => 'required',
-           'status'         => 'required',
-        ]);
     }
 }
--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          */
         'Intervention\Image\ImageServiceProvider',
         Alfa6661\AutoNumber\AutoNumberServiceProvider::class,
+        Nexmo\Laravel\NexmoServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -228,6 +229,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Image' => 'Intervention\Image\Facades\Image',
+        'Nexmo' => Nexmo\Laravel\Facade\Nexmo::class,
     ],
 
 ];
